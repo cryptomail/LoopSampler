cd00cdee05b2846bc690af2f2937c5648f116a6f
Joshua Teitelbaum <joshuat@gmail.com>
2021-12-26 08:08:47 -0800

    Initial Commit

---
+++ LoopSamplerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoopSamplerDemo
+//
+//  Created by Joshua Teitelbaum on 12/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
